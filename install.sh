1b0a9d04cb50150d211f291d0e46f41b5d49bee2
KillAura7170 <op_killaura@tutanota.com>
2023-06-06 18:10:46 +0900

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ lvcreate --contiguous y --size 8G system --name swap
 lvcreate --contiguous y --extents +100%FREE system --name root
 mkswap -L SWAP /dev/system/swap
 mkfs.ext4 -L ROOT /dev/system/root
+swapon /dev/system/swap
+mount /dev/system/root
+mkdir /mnt/boot
 pacstrap -K /mnt base base-devel
 pacstrap -K /mnt linux linux-firmware linux-headers
 pacstrap -K /mnt dnscrypt-proxy vim networkmanager ufw
